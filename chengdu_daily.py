82a42816d365cd49543d3928f4ff07710144d03a
fun <854814501@qq.com>
2025-05-10 22:48:54 +0800

    refactor(sichuan_daily.py): 移除冗余注释并简化标题获取逻辑 feat(chengdu_daily.py): 新增成都日报爬虫功能

--- chengdu_daily.py
+++ chengdu_daily.py
@@ -14,19 +14,16 @@ def getPageList(year, month, day):
     功能：获取当天报纸的各版面的链接列表
     参数：年，月，日
     '''
-    url = 'https://epaper.scdaily.cn/shtml/scrb/' + year + month + day + '/v01.shtml'
+    url = 'https://www.cdrb.com.cn/epaper/cdrbpc/' + year + month + '/' + day + '/l01.html'
     html = fetchUrl(url)
     bsobj = bs4.BeautifulSoup(html,'html.parser')
-    target_div = bsobj.find('div', attrs = {'class': 'main_r'})
-    # https://epaper.scdaily.cn/shtml/scrb/20250507/v01.shtml
-    # 第0个ul是二维码，第1个ul是版面导航，第2个ul是版面内容
-    target_ul = target_div.find_all('ul')[1]
-    pageList = target_ul.find_all('p')
+    target_div = bsobj.find('div', attrs = {'class': 'nav-list'})
+    pageList = target_div.find_all('li')
     linkList = []
 
     for page in pageList:
         link = page.a["href"]
-        url = 'https://epaper.scdaily.cn' + link
+        url = 'https://www.cdrb.com.cn/epaper/cdrbpc/' + year + month + '/' + day + '/' + link
         linkList.append(url)
 
     return linkList
@@ -38,18 +35,14 @@ def getTitleList(year, month, day, pageUrl):
     '''
     html = fetchUrl(pageUrl)
     bsobj = bs4.BeautifulSoup(html,'html.parser')
-    target_div = bsobj.find('div', attrs = {'class': 'main_r'})
-    # https://epaper.scdaily.cn/shtml/scrb/20250507/v01.shtml
-    # 第0个ul是二维码，第1个ul是版面导航，第2个ul是版面内容
-    target_ul = target_div.find_all('ul')[2]
-    titleList = target_ul.find_all('p')
+    target_div = bsobj.find('div', attrs = {'class': 'news-list'})
+
+    titleList = target_div.find_all('li')
     linkList = []
 
     for title in titleList:
-        target_a = title.find('a')
-        link = target_a["href"]
-        url = 'https://epaper.scdaily.cn' + link
-        linkList.append(url)
+        link = title.a["href"]
+        linkList.append(link)
 
     return linkList
 
@@ -59,34 +52,28 @@ def getContent(html):
     参数：html 网页内容
     '''
     bsobj = bs4.BeautifulSoup(html,'html.parser')
-    target_ul = bsobj.find('ul', attrs = {'class': 'news'})
+    target_div = bsobj.find('div', attrs = {'class': 'totalTitle'})
     # 获取文章 标题
-    h3_text = target_ul.h3.text if target_ul.h3 else ""
-    h1_text = target_ul.h1.text if target_ul.h1 else ""
-    h2_text = target_ul.h2.text if target_ul.h2 else ""
+    h3_text = target_div.h3.text if target_div.h3 else ""
+    h1_text = target_div.h1.text if target_div.h1 else ""
+    h2_text = target_div.h2.text if target_div.h2 else ""
     title = h3_text + '\n' + h1_text + '\n' + h2_text + '\n'
     #print(title)
 
     # 获取文章 内容
-    font_List = target_ul.find_all('font')
+    pList = bsobj.find('div', attrs = {'id': 'ozoom'}).find_all('p')
     content = ''
-    for font in font_List:
-        text_parts = []
-        for element in font.contents:
-            if element.name == 'br':  # 遇到 br 标签则添加换行
-                text_parts.append('\n')
-            else:  # 其他元素提取文本（自动处理标签嵌套）
-                text_parts.append(str(element).strip())  # strip() 去除多余空白（可选）
-        content += ''.join(text_parts) + '\n'
+    for p in pList:
+        content += p.text + '\n'
     #print(content)
 
     # 返回结果 标题+内容
     resp = title + content
     return resp
 
-def download_scrb(year, month, day, destdir):
+def download_cdrb(year, month, day, destdir):
     '''
-    功能：爬取《四川日报》网站 某年 某月 某日 的新闻内容，并保存在 指定目录下
+    功能：爬取《成都日报》网站 某年 某月 某日 的新闻内容，并保存在 指定目录下
     参数：年，月，日，文件保存的根目录
     '''
     pageList = getPageList(year, month, day)
@@ -121,12 +108,12 @@ if __name__ == '__main__':
     主函数：程序入口
     '''
     # 输入起止日期，爬取之间的新闻
-    print("欢迎使用四川日报爬虫，请输入以下信息：")
+    print("欢迎使用成都日报爬虫，请输入以下信息：")
     # beginDate = input('请输入开始日期:')
     # endDate = input('请输入结束日期:')
-    beginDate = '20250507'
-    endDate = '20250508'
-    destdir = "./data/scrb"
+    beginDate = '20250510'
+    endDate = '20250511'
+    destdir = "./data/cdrb"
     data = get_date_list(beginDate, endDate)
 
     for d in data:
@@ -135,9 +122,8 @@ if __name__ == '__main__':
         day = str(d.day) if d.day >=10 else '0' + str(d.day)
         destdir = destdir  # 爬下来的文件的存储地方
 
-        download_scrb(year, month, day, destdir)
+        download_cdrb(year, month, day, destdir)
         print("爬取完成：" + year + month + day)
         time.sleep(5)        # 怕被封 IP 爬一爬缓一缓，爬的少的话可以注释掉
 
     lastend = input("本次数据爬取完成!可以关闭软件了")
-
